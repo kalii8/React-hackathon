465923e582dbfdc740c13168eff6859195a35f4b
Sean Hochman <seanhochman@Seans-MacBook-Air.local>
2018-06-29 10:21:23 +0200

    header init

--- src/Header/header.js
+++ src/Header/header.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './header.css';
 
-export default class Header extends React.Compontent {
+export default class Header extends React.Component {
     render() {
-        
+        return (
+            <nav>
+                <div className="logo">FLIGHT SITE</div>
+                <div className="navItem">Search</div>
+                <div className="navItem">Browse</div>
+            </nav> 
+        )  
     }
 }
